6fc47ce0cc11bc531c12aa426d166e371a35b1c0
LeviMatias <matilevi35@gmail.com>
2021-10-08 19:42:27 -0300

    some changes

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use std::env;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
-use std::thread;
+use std::{thread, time};
 use std_semaphore::Semaphore;
 use std::collections::VecDeque;
+use std::sync::mpsc::{Sender, Receiver};
+use std::sync::{mpsc, Arc, Mutex};
 
 /* (archivo con de configuracion con rate limit y numero de aerolineas)
 / 1. inicializar web services de aerolineas y hotel (struct)
@@ -15,64 +17,51 @@ use std::collections::VecDeque;
 / 5. el cliente recibe el valor de retorno del metodo procesar y finaliza
 */
 
-pub struct Webservice {
-    notEmpty: Semaphore,
-    notFull: Semaphore,
-    requests: Queue<String>
-}
+
 
 pub struct Flight {
     pub origin: String,
     pub destination: String,
-    pub airline: String
+    pub airline: String,
+    pub result_gateway: Sender<FlightResult>
 }
 
 pub struct FlightResult {
-    pub flight: Flight,
+    pub id: String,
     pub accepted: bool
 }
 
+pub struct Webservice {
+    recv: Arc<Mutex<Receiver<Flight>>>,
+    send: Arc<Mutex<Sender<Flight>>>
+}
+
+fn process_flight(reservation: Flight){
+    thread::sleep(time::Duration::from_millis(100));
+    println!("Processing flight {} to {} from {}",reservation.airline, reservation.destination, reservation.origin);
+    let id_str = format!("{}{}{}", reservation.airline.to_owned(), reservation.destination, reservation.origin);
+    reservation.result_gateway.send( FlightResult{id: id_str, accepted: true} );
+}
+
 impl Webservice {
-    pub const fn new(rate_limit: u32) -> Webservice {
-        Webservice {
-            notEmpty: Semaphore(0),
-            notFull: Semaphore(rate_limit),
-            requests: VecDeque<Flight>::new()
-        }
-    }
-
     pub fn run_webservice(&self) {
+        let recver = self.recv.lock().unwrap();
         loop {
-            self.notEmpty.acquire();
-            let request = self.requests.pop_front();
-            self.consume(request);
-            self.notFull.release();
+           let flight = recver.recv().unwrap();
+            thread::spawn(move || process_flight(flight));
         }
-    }
-
-    pub fn consume(&self, request: Flight) -> FlightResult{
-        // hacer cosas importantes y utiles
-        FlightResult {
-            flight: request,
-            accepted: true,
-        }
-    }
-
-    pub fn process(&self, reservation: Flight) -> FlightResult {
-        self.notFull.acquire();
-        self.requests.append(reservation);
-        self.notEmpty.signal();
     }
 }
 
-
+fn create_webservice(rate_limit: u32) -> Webservice {
+        let (wbs_send, wbs_recv) = mpsc::channel();
+    Webservice { recv: Arc::new(Mutex::new(wbs_recv)), send: Arc::new(Mutex::new(wbs_send))}
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     const RATE_LIMIT: u32 = 1;
-
-    let flight_ws: Webservice = Webservice::new(RATE_LIMIT);
 
 
     let filename = &args[1];
@@ -80,17 +69,33 @@ fn main() {
     let reader = BufReader::new(file);
 
     let mut threads = vec![];
-    threads.push(thread::spawn(|| { flight_ws.run_webservice() }));
+    let flight_ws = Arc::new(create_webservice(RATE_LIMIT));
+
+    let ws_copy = flight_ws.clone();
+    threads.push(thread::spawn(move||  ws_copy.run_webservice() ));
 
 
-    for line in reader.lines().flatten() {
+    //for line in reader.lines().flatten() {
+        let (result_send, result_recv) = mpsc::channel();
+        let wbs_message_sender = flight_ws.send.clone();
+
         let reservation = Flight {
-            origin: "APQ",
-            destination: "BRC",
-            airline: "aerolineas argentinas"
-        }
-        threads.push(thread::spawn(move |reservation| flight_ws.process(reservation) ));
-    }
+            origin: "APQ".to_owned(),
+            destination: "BRC".to_owned(),
+            airline: "aerolineas argentinas".to_owned(),
+            result_gateway: result_send
+        };
+        threads.push(thread::spawn(move || {
+            // acquire lock
+            let sender = wbs_message_sender.lock().unwrap();
+            sender.send(reservation);
+            drop(sender);
+            //release lock
+
+            let result = result_recv.recv().unwrap();
+            println!("Flight {} has been proccessed with result {}", result.id, result.accepted);
+        }));
+    //}
 
     for thread in threads {
         thread
@@ -99,10 +104,3 @@ fn main() {
     }
 }
 
-fn parse(line: String) -> Flight{
-
-}
-
-fn process(text: String) {
-    println!("{}", text);
-}
