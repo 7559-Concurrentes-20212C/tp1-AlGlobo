18288aa9ef3ba68f4bf9ac6a822f4a88e86b3970
LeviMatias <matilevi35@gmail.com>
2021-10-24 15:52:22 -0300

    result service processes reservation result

--- src/resultservice.rs
+++ src/resultservice.rs
@@ -1,14 +1,14 @@
 use crate::thread_pool;
 use crate::reservation;
 
-use std::sync::Mutex;
+use std::sync::{Mutex, Arc};
 use reservation::{ReservationResult};
 use thread_pool::{ThreadPool};
 use crate::stats_service::{StatsService, MovingStats};
 
 pub struct ResultService {
     pub thread_pool : Mutex<ThreadPool>,
-    stats: StatsService,
+    stats: Arc<StatsService>,
 }
 
 impl ResultService {
@@ -16,21 +16,27 @@ impl ResultService {
     pub fn new(rate_limit: usize) -> ResultService {
         ResultService {
             thread_pool : Mutex::new(ThreadPool::new(rate_limit)),
-            stats: StatsService::new(rate_limit, 1000)
+            stats: Arc::new(StatsService::new(rate_limit, 1000))
         }
     }
 
-    pub fn process_result(&self, id_str : String, accepted : bool) {
-
-        print!("procesando el id: {}", id_str);
-
-        self.thread_pool.lock().unwrap().execute(|| { // TODO validar unwrap
-            ReservationResult::new(id_str, true);
+    pub fn process_result(&self, reservation: ReservationResult) {
+        let stats = self.stats.clone();
+        self.thread_pool.lock().expect("could not acquire thread").execute(move || { // TODO estaria bueno que escriba en un archivo tmb
+            print!("processing result with id: {}", reservation.id);
+            stats.process_result_stats( reservation );
         });
 
     }
 
     pub fn get_stats_history(&self) -> MovingStats {
         return self.stats.calculate_stats();
+    }
+
+    pub fn print_results(&self) {
+        let stats = self.stats.calculate_stats();
+        println!("sample size {}", stats.sample_size);
+        println!("avg latency {}", stats.avg_latency);
+        println!("success rate {}", stats.success_rate);
     }
 }
