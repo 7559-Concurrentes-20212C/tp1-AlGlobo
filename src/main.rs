c4b3338330d0731f7a28a3a7e29dbbb2fe856dbd
LeviMatias <matilevi35@gmail.com>
2021-10-24 15:54:28 -0300

    tweaks and file error handling

--- src/main.rs
+++ src/main.rs
@@ -17,36 +17,46 @@ use std::io::BufRead;
 use std::io::BufReader;
 use std::thread;
 
-use crate::reservation::ReservationKind;
-
 fn main() {
 
     let args: Vec<String> = env::args().collect();
 
     const RATE_LIMIT: usize = 4;
+    const SUCCESS_CHANCE : usize = 70;
     let reservations : String = String::from("reservations.txt");
 
     // let filename = &args[0];
-    let file = File::open(reservations).expect("could not open file");
+    let f = File::open(reservations);
+    let file = match f {
+        Ok(file) => file,
+        Err(error) => {
+            println!("problem opening file: {:?}", error);
+            return;},
+    };
     let reader = BufReader::new(file);
 
     //creates service for handling incoming results
     let results_service = Arc::new(ResultService::new(RATE_LIMIT));
 
     //creates all web services container
-    let mut web_services : HashMap<Arc<String>, Webservice> = HashMap::new();
+    let mut web_services : HashMap<String, Webservice> = HashMap::new();
 
     //creates hotel
-    let hotel = Arc::new(Hotel::new());
+    //let hotel = Arc::new(Hotel::new());
 
     for line in reader.lines().flatten() {
-        let reservation = Reservation::new(line, Arc::clone(&results_service));
-        let ws = web_services.entry(Arc::clone(&reservation.airline)).or_insert(Webservice::new(RATE_LIMIT));
+        let reservation = Arc::new(Reservation::from_line(line, Arc::clone(&results_service)));
+        let ws = web_services.entry(reservation.airline.clone())
+                                            .or_insert(Webservice::new(RATE_LIMIT, SUCCESS_CHANCE));
 
-        ws.process(reservation);
-        if matches!(reservation.kind, ReservationKind::Package){hotel.process(reservation);}
+        ws.process(reservation.clone());
+        //todo, esto esta mal, se tiene que evaluar todo junto el paquete (no podemos aceptar hotel si y vuelo no)
+        //todo no se me ocurre como hacerlo con este modelo, voy a pensar
+        //if matches!(reservation.kind, ReservationKind::Package){hotel.process(reservation.clone());}
     }
 
+    //si aca habria que hacer esperar al main, por ahi que lea de consola un x o algo
     thread::sleep(time::Duration::from_millis(15000)); //TODO si no pongo esto el main sale de scope y ordena a terminar el resto de los threads
     println!("finished!");
+    results_service.print_results();
 }
