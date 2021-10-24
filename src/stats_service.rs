8ba1e81b57b78d335c996b7579a82b7a5dc69073
LeviMatias <matilevi35@gmail.com>
2021-10-24 15:49:26 -0300

    stats service extended calculations

--- src/stats_service.rs
+++ src/stats_service.rs
@@ -1,14 +1,16 @@
 use std::sync::{Mutex, Arc};
 use crate::thread_pool::ThreadPool;
 use crate::resultservice::ResultService;
-use crate::flight::FlightResult;
 use std::collections::VecDeque;
+use crate::reservation::ReservationResult;
+use std::cmp::{max, min};
 
 pub struct StatsService {
     pub thread_pool : Mutex<ThreadPool>,
-    history: Arc<Mutex<VecDeque<FlightResult>>>,
+    history: Arc<Mutex<VecDeque<ReservationResult>>>,
 }
 
+//its called moving stats because it return stats for a moving window of max size history.capacity
 pub struct MovingStats {
     pub sample_size: usize,
     pub success_rate: f32,
@@ -23,11 +25,11 @@ impl StatsService {
         let thread_pool = Mutex::new(ThreadPool::new(rate_limit));
         StatsService {
             thread_pool,
-            history :  Arc::new((Mutex::new(VecDeque::with_capacity(moving_avg)))),
+            history :  Arc::new(Mutex::new(VecDeque::with_capacity(moving_avg))),
         }
     }
 
-    pub fn process_result_stats(&self, f :FlightResult) {
+    pub fn process_result_stats(&self, f :ReservationResult) {
         let history = self.history.clone();
 
         self.thread_pool.lock().expect("could not get thread lock! stats_service").execute(move || {
@@ -43,14 +45,17 @@ impl StatsService {
         let mut sample_size = 0;
         let mut success_rate = 0.0;
         let mut avg_latency = 0.0;
-        let mut highest_latency = 0.0;
-        let mut lowest_latency = 0.0;
+        let mut highest_latency :f32 = 0.0;
+        let mut lowest_latency :f32 = f32::MAX;
 
         for val in self.history.lock().expect("could not load history").iter() {
             sample_size += 1;
             if val.accepted {
                 success_rate += 1.0;
             }
+            avg_latency += val.time_to_process.as_secs_f32();
+            highest_latency = highest_latency.max(val.time_to_process.as_secs_f32());
+            lowest_latency = lowest_latency.min(val.time_to_process.as_secs_f32())
         }
         if sample_size == 0{
             return MovingStats{
