30d94eb7c9a510ddd8744418b6489ef35f463dc7
LeviMatias <matilevi35@gmail.com>
2021-10-25 01:29:05 -0300

    removed unnecessary threads

--- src/schedule_service.rs
+++ src/schedule_service.rs
@@ -5,6 +5,7 @@ use crate::reservation::{Reservation, ReservationKind, ReservationResult, Reserv
 use crate::thread_pool::ThreadPool;
 use crate::resultservice::ResultService;
 use std::time::Duration;
+use std::thread::Thread;
 
 pub struct ScheduleService {
     thread_pool : ThreadPool,
@@ -32,22 +33,20 @@ impl ScheduleService {
         let result_service = self.result_service.clone();
 
         self.thread_pool.execute(move || {
-            let (sender, receiver) = mpsc::channel();
-            let reservation_req = Arc::new(ReservationProcessRequest::new(reservation.clone(), Arc::new(Mutex::new(sender))));
-
             match reservation.kind {
                 ReservationKind::Flight => {
-                    webservice.process(reservation_req);
-                    let result = receiver.recv().unwrap(); //todo err handling
+                    let result = webservice.process(reservation);
                     result_service.process_result(result);
 
                     return;
                 }
                 ReservationKind::Package => {
-                    webservice.process(reservation_req.clone());
-                    hotel_webservice.process(reservation_req.clone());
-                    let r1 = receiver.recv().unwrap(); //todo err handling
-                    let r2 = receiver.recv().unwrap();
+                    let hotel_res = reservation.clone();
+                    let r1 = Thread::spawn(move ||{
+                        return webservice.process(hotel_res.clone())
+                    } );
+                    let r2 = hotel_webservice.process(reservation.clone());
+                    let r1 = r1.join().unwrap();
 
                     let duration = Duration::from_secs_f32(
                         r1.time_to_process.as_secs_f32().max(r2.time_to_process.as_secs_f32()));
--- src/webservice.rs
+++ src/webservice.rs
@@ -16,15 +16,13 @@ enum Decision {
 }
 
 pub struct Webservice {
-    thread_pool: ThreadPool,
     success_rate: usize
 }
 
 impl Webservice {
 
-    pub fn new(capacity: usize, success_chance: usize) -> Webservice {
+    pub fn new(success_chance: usize) -> Webservice {
         Webservice {
-            thread_pool: ThreadPool::new(capacity),
             success_rate: success_chance % 100
         }
     }
@@ -37,36 +35,18 @@ impl Webservice {
         return Decision::Rejected;
     }
 
-    pub fn process(&self, req: Arc<ReservationProcessRequest>){
+    pub fn process(&self, req: Arc<Reservation>) -> ReservationResult {
         let start = Instant::now();
         let decision = self.decide();
-        self.thread_pool.execute(move || {
-            let id = _process(req.reservation.clone());
 
-            random_wait();
-            let result = ReservationResult::from_reservation_ref(req.reservation,
-                                                matches!(decision , Decision::Accepted),
-                                                start.elapsed());
-            req.resolve(result);
-        })
+        random_wait();
+        let result = ReservationResult::from_reservation_ref(req,
+                                            matches!(decision , Decision::Accepted),
+                                            start.elapsed());
+        return result;
     }
 }
 
-fn _process(reservation: Arc<Reservation>) -> String {
-
-    println!(
-        "Processing flight {} to {} from {}",
-        reservation.airline, reservation.destination, reservation.origin
-    );
-    let id_str = format!(
-        "{}{}{}",
-        reservation.airline,
-        reservation.destination,
-        reservation.origin,
-    );
-
-    return id_str;
-}
 
 fn wait(miliseconds: u64){
     thread::sleep(time::Duration::from_millis(miliseconds));
